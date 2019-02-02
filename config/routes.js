54c5263aa0d4363c70f1ff8788898fd1b59a7e7b
Josh Z <jzieger10@gmail.com>
2019-02-02 16:38:45 -0500

    deleted an unneeded get function

--- config/routes.js
+++ config/routes.js
@@ -69,23 +69,3 @@ function login(req, res) {
 			})
 		);
 }
-
-function getJokes(req, res) {
-	const requestOptions = {
-		headers: {
-			accept: "application/json",
-		},
-	};
-	console.log(requestOptions);
-	axios
-		.get("https://icanhazdadjoke.com/search", requestOptions)
-		.then(response => {
-			res.status(200).json(response.data.results);
-		})
-		.catch(err => {
-			res.status(500).json({
-				message: "Error Fetching Jokes",
-				error: err,
-			});
-		});
-}

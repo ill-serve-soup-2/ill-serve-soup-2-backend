8d8cd3d561df84e1e9309e9eedee8ab9d5fe4a37
Josh Z <jzieger10@gmail.com>
2019-02-03 12:05:52 -0500

    testing deployment settings

--- knexfile.js
+++ knexfile.js
@@ -30,17 +30,17 @@ module.exports = {
 
 	production: {
 		client: "postgresql",
-		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
-		},
+		connection: process.env.DATABASE_URL,
+		// database: "my_db",
+		// user: "username",
+		// password: "password",
+
 		pool: {
 			min: 2,
 			max: 10,
 		},
 		migrations: {
-			tableName: "knex_migrations",
+			tableName: "dbmigrations",
 		},
 	},
 };
